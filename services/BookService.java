aeff6bb9dd18c1458e226ef0ca396dc0071d1b18
vashvi singh <163646805+Vashvwee@users.noreply.github.com>
2024-07-17 18:13:24 +0530

    Create BookService.java
    Services

---
+++ services/BookService.java
@@ -0,0 +1,42 @@
+package com.example.bookservice.service;
+
+import com.example.bookservice.model.Book;
+import com.example.bookservice.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Long id, Book book) {
+        Book existingBook = bookRepository.findById(id).orElse(null);
+        if (existingBook != null) {
+            existingBook.setTitle(book.getTitle());
+            existingBook.setAuthor(book.getAuthor());
+            existingBook.setIsbn(book.getIsbn());
+            return bookRepository.save(existingBook);
+        }
+        return null;
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
